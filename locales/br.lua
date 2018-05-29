bb968f80b4409c90f6e544f3b7a90fd18a1c23a0
renildomarcio <krsolucoesweb@gmail.com>
2018-05-29 04:54:49 -0300

    Create br.lua
    Update for BR Brazil translation

---
+++ locales/br.lua
@@ -0,0 +1,61 @@
+Locales['br'] = {
+  -- Cloakroom
+  ['vehicle_list'] = 'Lista de veículos',
+  ['civil'] = 'roupas civis',
+  ['job_clothes'] = 'roupas de trabalho',
+  ['foodtruck_actions_menu'] = 'pressione ~INPUT_CONTEXT~ para acessar o menu.',
+  
+  -- Vehicles
+  ['store_veh'] = 'pressione ~INPUT_CONTEXT~ para guardar o veículo ' ,
+  ['wrong_veh'] = 'Você tem que estar em um ~b~Caminhão de comida~s~.',
+  ['vehicles'] = 'veículos',
+
+  -- Menus
+  ['recipe'] = 'receita',
+  ['ingredients'] = 'Ingredientes',
+  ['price_unit'] = '$ / unidade',
+  ['on_you'] = 'em você',
+  ['action'] = 'açao',
+  ['buy_10'] = 'Comprar 10',
+  ['buy_50'] = 'Comprar 50',
+  ['cook'] = 'preparar',
+  ['billing'] = 'faturamento',
+  ['gears'] = 'material',
+  ['purchased'] = 'Compra feita.',
+
+  -- Boss Menu
+  ['boss_actions'] = 'ações de chefe',
+
+  -- Food
+  ['grill'] = 'grade',
+  ['table'] = 'mesa',
+  ['chair'] = 'cadeira',
+  ['clean'] = 'Limpar',
+  ['bread'] = 'pão',
+  ['meat'] = 'carne',
+  ['vegetables'] = 'legumes',
+  ['tacos'] = 'tacos',
+  ['burger'] = 'hambúrguer',
+  ['drank_coke'] = 'Você bebeu uma ~b~Coca-cola',
+  ['eat_burger'] = 'Você comeu um ~b~hambúrguer',
+  ['eat_taco'] = 'Você comeu um ~b~Taco',
+  -- F6 Menu
+
+  -- Phone
+  ['alert_foodtruck'] = 'Alerta do cliente',
+  -- MISC
+  ['blip_market'] = 'mercado',
+  ['blip_foodtruck'] = 'caminhão de comida',
+  ['take'] = 'pressione ~INPUT_CONTEXT~ para pegar',
+  ['clean_too_far'] = 'você está muito longe de seu equipamento para guardá-lo.',
+  ['cooked'] = 'cozinhar feito!',
+  ['no_player_nearby'] = 'Nenhum jogador por perto',
+  ['bill_amount'] = 'Montante da fatura',
+  ['missing_ingredients'] = 'você está perdendo ingredientes!',
+  ['already_cooking'] = 'uma receita já está em preparação!',
+  ['start_cooking_hint'] = 'press ~INPUT_CONTEXT~ para cozinhar.',
+  ['need_more_exp'] = 'Você não está ~r~ com ~s~ experiente o suficiente para executar essa ação.',
+  ['foodtruck_menu'] = 'pressione ~INPUT_CONTEXT~ para acessar o menu.',
+  ['foodtruck_market_menu'] = 'press ~INPUT_CONTEXT~ para acessar o mercado.',
+  ['no_money'] = 'Você não tem dinheiro suficiente!',
+}
